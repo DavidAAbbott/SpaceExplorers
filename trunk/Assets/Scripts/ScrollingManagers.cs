fac2b9a3d13024265cff2a50b8ece2af57fd9797
Mugsworth <Mugsworth@3b6de723-ae5c-463c-b183-8107edd061b4>
2015-02-17 13:50:37 +0000

    Sama ku edellinen, hommat kusi

---
+++ trunk/Assets/Scripts/ScrollingManagers.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class ScrollingObject
+{
+    public GameObject obj;
+    public float speed;
+}
+public class ScrollingManagers : MonoBehaviour
+{
+    public ScrollingObject[] scrollingObj;
+    public ScrollingObject lastScrollingObj;
+    public Vector3 startPos;
+
+    void Start()
+    {
+        startPos = lastScrollingObj.obj.transform.position;
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < scrollingObj.Length; ++i)
+        {
+               scrollingObj[i].obj.transform.position -= Vector3.right * scrollingObj[i].speed * Time.deltaTime;
+        }
+
+        for (int i = 0; i < scrollingObj.Length; ++i)
+        {
+            if (scrollingObj[i].obj.transform.position.x <= -14.4f)
+            {
+                scrollingObj[i].obj.transform.position = new Vector3(lastScrollingObj.obj.transform.position.x + lastScrollingObj.obj.renderer.bounds.size.x, scrollingObj[i].obj.transform.position.y);
+                lastScrollingObj = scrollingObj[i];
+            }
+        }
+    }
+}
