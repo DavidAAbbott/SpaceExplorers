52e14eb539f58e7b22992ad7bf02e71a6a31ad62
Mugsworth <Mugsworth@3b6de723-ae5c-463c-b183-8107edd061b4>
2015-03-30 01:54:25 +0000

    lasers and stuff

---
+++ trunk/Assets/Scripts/LaserScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserScript : MonoBehaviour {
+    private GameObject laser;
+    private GameObject laserStartCast;
+
+	void Start () {
+        laser = GameObject.Find("Laser");
+        laserStartCast = GameObject.Find("LaserStartCast");
+        laser.SetActive(false);
+        laserStartCast.SetActive(false);
+
+        StartCoroutine("timer");
+	}
+	
+	void Update () {
+	}
+    IEnumerator timer()
+    {
+        laserStartCast.SetActive(true);
+        yield return new WaitForSeconds(0.9f);
+        laser.SetActive(true);
+        //laserStartCast.SetActive(false);
+    }
+}
