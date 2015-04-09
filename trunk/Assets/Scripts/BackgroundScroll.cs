8937b124bcbba251a86f9c4a37a52e912d293f6d
Mugsworth <Mugsworth@3b6de723-ae5c-463c-b183-8107edd061b4>
2015-04-09 04:56:11 +0000

    BackgroundScroll

---
+++ trunk/Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class BackgroundScroll : MonoBehaviour
+{
+    private Vector2 velocity;
+    public float scrollspeed = 0.1f;
+    private Rigidbody2D player;
+    private Renderer rendererBG;
+    public bool moving = true;
+
+    void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<Rigidbody2D>();
+        rendererBG = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        if(moving)
+        {
+            velocity = player.velocity;
+            Vector2 scroll = rendererBG.material.mainTextureOffset;
+            scroll += new Vector2(scrollspeed * velocity.x * Time.deltaTime, 0);
+            rendererBG.material.mainTextureOffset = scroll;
+        }
+    }
+}
