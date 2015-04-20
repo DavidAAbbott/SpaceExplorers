35a58f3ec6bc7ae0711f028b930d1749cb92359d
finndave <finndave@3b6de723-ae5c-463c-b183-8107edd061b4>
2015-04-20 07:44:21 +0000

    Open World planet entering functionality and UI

--- trunk/Assets/Scripts/Map/EnterPlanet.cs
+++ trunk/Assets/Scripts/Map/EnterPlanet.cs
@@ -1,33 +1,72 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
-public class EnterPlanet : MonoBehaviour {
-    public GUIText menu;
+public class EnterPlanet : MonoBehaviour
+{
+    public Text openMenu;
+    public Text planet;
+    public Text planetDesc;
+    public Text enter;
+    public Text exit;
+    public Image menuBG;
     private bool intrigger = false;
+    private bool menushowing = false;
 
     void Start()
     {
-        menu.enabled = false;
+        openMenu.enabled = false;
+        planet.enabled = false;
+        planetDesc.enabled = false;
+        menuBG.enabled = false;
+        enter.enabled = false;
+        exit.enabled = false;
     }
+
     void Update()
     {
-        if (Input.GetButton("X_1") && intrigger == true)
+        if (Input.GetButtonUp("A_1") && intrigger == true)
+        {
+            openMenu.enabled = false;
+            menushowing = true;
+            planet.enabled = true;
+            planetDesc.enabled = true;
+            menuBG.enabled = true;
+            enter.enabled = true;
+            exit.enabled = true;
+            Time.timeScale = 0f; 
+        }
+
+        if (Input.GetButton("A_1") && menushowing == true)
         {
             Application.LoadLevel(2);
+            Time.timeScale = 1f;
+        }
+
+        if (Input.GetButton("B_1") && menushowing == true)
+        {
+            menushowing = false;
+            planet.enabled = false;
+            planetDesc.enabled = false;
+            menuBG.enabled = false;
+            enter.enabled = false;
+            exit.enabled = false;
+            Time.timeScale = 1f;
         }
     }
+
     void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.tag == "Player")
         {
-            menu.enabled = true;
             intrigger = true;
+            openMenu.enabled = true;
         }
     }
 
     void OnTriggerExit2D(Collider2D collider)
     {
         intrigger = false;
-        menu.enabled = false;
+        openMenu.enabled = false;
     }
 }
